4cf4c330966766df6681311ffbf3ce0a4f4d3b35
Luiz Augusto <dev.luiz.augusto@gmail.com>
2023-01-09 16:19:54 -0300

    AboutView

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,34 +1,60 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemProduto;
-	
+
 	@FXML
 	private MenuItem menuItemSobre;
-	
+
 	@FXML
 	public void onMenuItemProdutoAction() {
 		System.out.println("onMenuItemProdutoAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemSobreAction() {
-		System.out.println("onMenuItemSobreAction");
+		loadView("/gui/About.fxml");
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-	
-	}
-	
 
+	}
+
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVbBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVbox.getChildren().get(0);
+			mainVbox.getChildren().clear();
+			mainVbox.getChildren().add(mainMenu);
+			mainVbox.getChildren().addAll(newVbBox.getChildren());
+			
+		} catch (IOException e) {
+			Alerts.showAlerts("IO Exception", "Error ", e.getMessage(), AlertType.ERROR);
+		}
+
+	}
 }
