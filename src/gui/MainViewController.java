9aeb6409db4c0e1abddcce564df7e756cdc91309
Luiz Augusto <dev.luiz.augusto@gmail.com>
2023-01-09 15:20:06 -0300

    MainView controller

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,34 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+
+	@FXML
+	private MenuItem menuItemProduto;
+	
+	@FXML
+	private MenuItem menuItemSobre;
+	
+	@FXML
+	public void onMenuItemProdutoAction() {
+		System.out.println("onMenuItemProdutoAction");
+	}
+	
+	@FXML
+	public void onMenuItemSobreAction() {
+		System.out.println("onMenuItemSobreAction");
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+	
+	}
+	
+
+}
